4becbb455709491188fdd22edef797c67d47b05e
Phillip Garcia <phillipthomasgarcia@gmail.com>
2016-08-29 15:01:57 -0500

    adding views

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('partials.head')
+</head>
+<body>
+<header class="row">
+    @include('partials.header')
+</header>
+<div class="container">
+    @include('partials.sidebar')
+    <div id="main" class="row">
+
+        @yield('content')
+        @if (session()->has('message'))
+            <div class="alert alert-success">{{ session('successMessage') }}</div>
+        @endif
+    </div>
+
+</div>
+</body>
+</html>
