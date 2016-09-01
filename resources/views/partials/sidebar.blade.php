573c3ce1287643762d53304159852ee129149d0e
Phillip Garcia <phillipthomasgarcia@gmail.com>
2016-09-01 11:46:17 -0500

    seperating views and tasks

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,9 +1,9 @@
 <div class="nav-collapse sidebar-nav">
     <ul class="nav nav-tabs nav-stacked main-menu">
-        <li><a href="#"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Profile</span></a></li>
-        <li><a href="#"><i class="icon-envelope"></i><span class="hidden-tablet"> activities</span></a></li>
-        <li><a href="#"><i class="icon-tasks"></i><span class="hidden-tablet"> Schools</span></a></li>
-        <li><a href="#"><i class="icon-eye-open"></i><span class="hidden-tablet"> Tasks</span></a></li>
-        <li><a href="#"><i class="icon-dashboard"></i><span class="hidden-tablet"> Essays</span></a></li>
+        <li><a href="#"><i class="icon-user"></i><span class="hidden-tablet"> Profile</span></a></li>
+        <li><a href="#"><i class="icon-star"></i><span class="hidden-tablet"> Activities</span></a></li>
+        <li><a href="#"><i class="icon-file"></i><span class="hidden-tablet"> Schools</span></a></li>
+        <li><a href="#"><i class="icon-tasks"></i><span class="hidden-tablet"> Tasks</span></a></li>
+        <li><a href="/student/writeAnEssay"><i class="icon-folder-open"></i><span class="hidden-tablet"> Essays</span></a></li>
     </ul>
 </div>
