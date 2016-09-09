79d7a2e24adf129cb277261f60d76ea21a7dc041
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-09-08 19:41:01 -0500

    changes

--- database/seeds/essaySeeder.php
+++ database/seeds/essaySeeder.php
@@ -11,6 +11,6 @@ class essaySeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Essay::class, 20)->create();
+        // factory(App\Essay::class, 20)->create();
     }
 }
