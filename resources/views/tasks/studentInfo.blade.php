fa6445e45cf1ae19d2c0d6e08ffdf9f24a9ea53a
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-09-12 11:07:05 -0500

    changes to counselor view

--- resources/views/tasks/studentInfo.blade.php
+++ resources/views/tasks/studentInfo.blade.php
@@ -8,19 +8,19 @@
         <div class="schoolbox">
             <img src="http://lorempixel.com/460/250/" class="img-responsive">
             <div class="schooltitle">Preferred Name: {{ $student->preferred_name }}</div>
-            <p class="text-justify">Name: {{ $student->name }}</p>
-            <p class="text-justify">Gender: {{ $student->gender }}</p>
-            <p class="text-justify">Email: {{ $student->email }}</p>
-            <p class="text-justify">Phone:: {{ $student->phone }}</p>
-            <p class="text-justify">WeChat: {{ $student->personalInfo->WeChat }}</p>
-            <p class="text-justify">DOB: {{ $student->personalInfo->DOB }}</p>
-            <p class="text-justify">Parent: {{ $student->personalInfo->parent1 }}</p>
-            <p class="text-justify">Parent: {{ $student->personalInfo->parent2 }}</p>
-            <p class="text-justify">High School: {{ $student->personalInfo->highschool }}</p>
-            <p class="text-justify">SAT: {{ $student->personalInfo->SAT }}</p>
-            <p class="text-justify">ACT: {{ $student->personalInfo->ACT }}</p>
-            <p class="text-justify">TOEFL: {{ $student->personalInfo->TOEFL }}</p>
-            <p class="text-justify">GPA: {{ $student->personalInfo->GPA }}</p>
+            <p class="text-justify"><b>Name:</b> {{ $student->name }}</p>
+            <p class="text-justify"><b>Gender:</b> {{ $student->gender }}</p>
+            <p class="text-justify"><b>Email:</b> {{ $student->email }}</p>
+            <p class="text-justify"><b>Phone:</b> {{ $student->phone }}</p>
+            <p class="text-justify"><b>WeChat:</b> {{ $student->personalInfo->WeChat }}</p>
+            <p class="text-justify"><b>DOB:</b> {{ $student->personalInfo->DOB }}</p>
+            <p class="text-justify"><b>Parent:</b> {{ $student->personalInfo->parent1 }}</p>
+            <p class="text-justify"><b>Parent:</b> {{ $student->personalInfo->parent2 }}</p>
+            <p class="text-justify"><b>High School:</b> {{ $student->personalInfo->highschool }}</p>
+            <p class="text-justify"><b>SAT:</b> {{ $student->personalInfo->SAT }}</p>
+            <p class="text-justify"><b>ACT:</b> {{ $student->personalInfo->ACT }}</p>
+            <p class="text-justify"><b>TOEFL:</b> {{ $student->personalInfo->TOEFL }}</p>
+            <p class="text-justify"><b>GPA:</b> {{ $student->personalInfo->GPA }}</p>
             {{--<a href="{{ action("SchoolsController@addToMySchools", ['school_id'=> $student->id]) }}"><div class="pull-right"><button class="btn btn-success btn-sm">Add to My Schoools</button></div></a>--}}
         </div>
     </div>
