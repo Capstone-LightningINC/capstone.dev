4bd8cc0719c80f7070b11aea6311359b88dc5a1c
Phillip Garcia <phillipthomasgarcia@gmail.com>
2016-09-12 09:40:28 -0500

    fixed schools image rendering on page

--- resources/views/schools/school.blade.php
+++ resources/views/schools/school.blade.php
@@ -6,7 +6,11 @@
 
     <div class="col-md-2">
         <div class="schoolbox">
-            <img src="img/school_img {{ $school->img_url }}" class="img-responsive">
+            @if($school->img_url)
+            <img src="/img/schools_img/{{ $school->img_url }}" class="img-responsive">
+            @else
+                <img src="/img/schools_img/college.jpg" class="img-responsive">
+            @endif
             <div class="schooltitle">{{ $school->biz_name }}</div>
             <p class="text-justify"><b>Website: </b><a href="{{ $school->web_url }}" target="_blank">{{ $school->web_url }}</a></p>
             <p class="text-justify"><b>Address:</b> {{ $school->e_address }}</p>
