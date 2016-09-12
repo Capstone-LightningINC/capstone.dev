52a91b394f2569664fa2e4b15e87e314a3c9fbc5
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-09-12 09:05:47 -0500

    Added college stock photo to take place of lorempixel link

--- resources/views/schools/search.blade.php
+++ resources/views/schools/search.blade.php
@@ -33,7 +33,7 @@
     <a href="/schools/{{ $school->school_id }}">
     <div class="col-md-2">
       <div class="schoolbox">
-        <img src="http://lorempixel.com/460/250/" class="img-responsive">
+        <img src="/img/college.jpg" class="img-responsive">
         <div class="schooltitle">{{ $school->biz_name }}</div>
         <p class="text-justify form-control"><a href="{{ $school->web_url }}" target="_blank">{{ $school->web_url }}</a></p>
         <div><button class="btn btn-success btn-sm more-info">More Info</button></div>
