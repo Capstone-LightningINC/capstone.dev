8ba01bdff447a12ae3b73e39f3f1a55b0d0b72a2
Phillip Garcia <phillipthomasgarcia@gmail.com>
2016-09-13 11:31:23 -0500

    took out profile pic momentarily

--- resources/views/tasks/studentInfo.blade.php
+++ resources/views/tasks/studentInfo.blade.php
@@ -6,7 +6,7 @@
 
     <div class="col-md-2">
         <div class="personalInfoBox">
-            <img src="/img/profile_img/{{ $student->profile_img }}" class="img-responsive">
+            {{--<img src="/img/profile_img/{{ $student->profile_img }}" class="img-responsive">--}}
             <div class="schooltitle" id="namePadding">{{ $student->preferred_name }}</div>
             <div class="row-fluid">
             <div class="infoBox">
